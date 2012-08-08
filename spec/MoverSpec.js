2924ee447502c6fabe6a15bcf6eaf5dbd4c94a79
Truls Tangstad <tangstad@pvv>
2012-08-08 18:32:40 +0900

    can add one single waypoint

--- spec/MoverSpec.js
+++ spec/MoverSpec.js
@@ -12,7 +12,45 @@ describe("Mover", function() {
 		mover.tick();
 		expect(mover.locationX).toEqual(0);
 		expect(mover.locationY).toEqual(0);
-		
+	});
+
+	it("remains still right after setting waypoint", function() {
+		var mover = new Mover();
+
+		mover.addWaypoint(10,10,1);
+
+		expect(mover.locationX).toEqual(0);
+		expect(mover.locationY).toEqual(0);
+	});
+
+	it("can move to next waypoint", function() {
+		var mover = new Mover();
+
+		mover.addWaypoint(10,10,1);
+		mover.tick();
+
+		expect(mover.locationX).toEqual(10);
+		expect(mover.locationY).toEqual(10);
+	});
+
+	it("can move to different waypoint", function() {
+		var mover = new Mover();
+
+		mover.addWaypoint(20,20,1);
+		mover.tick();
+
+		expect(mover.locationX).toEqual(20);
+		expect(mover.locationY).toEqual(20);
+	});
+
+	it("can move one step towards waypoint", function() {
+		var mover = new Mover();
+
+		mover.addWaypoint(10,10,2);
+		mover.tick();
+
+		expect(mover.locationX).toEqual(5);
+		expect(mover.locationY).toEqual(5);
 	});
 });
 
--- src/Mover.js
+++ src/Mover.js
@@ -1,7 +1,22 @@
 var Mover = function() {
 	this.locationX = 0;
 	this.locationY = 0;
+
+	var waypointX = 0;
+	var waypointY = 0;
+	var steps;
+
 	this.tick = function() {
+		if (waypointX != 0) {
+			this.locationX = (waypointX / steps);
+			this.locationY = (waypointY / steps);
+		}
+	};
+
+	this.addWaypoint = function(x,y,s) {
+		waypointX = x;
+		waypointY = y;
+		steps = s;
 	};
 };
 
